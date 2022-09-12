de6674448398be6e190304de98d44615810c24b6
vetrivel <vetrivel.kce@gmail.com>
2022-09-12 14:19:14 +0530

    welcome to advance address book problem

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Data;
+
+namespace AddressBookProblem_LINQ_DAY35
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to AddressBook Problem for LINQ");
+
+            
+        }
+    }
+}
