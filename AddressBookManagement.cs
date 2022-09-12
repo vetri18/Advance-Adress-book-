55be6c6935bb9381fee8291f4043ca5c0ecc593d
vetri18 <109862568+vetri18@users.noreply.github.com>
2022-09-12 14:25:48 +0530

    UC4_EditingExistingContacts

---
+++ AddressBookManagement.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace AddressBookProblem_LINQ_DAY35
+{
+    public class AddressBookManagement
+    {
+
+        /// <summary>
+        /// UC4- Updating existing contact details.
+        /// </summary>
+        /// <param name="dataTable"></param>
+        /// <returns></returns>
+        public DataTable UpdatedContactDetails(DataTable dataTable)
+        {
+            var recordData = dataTable.AsEnumerable().Where(a => a.Field<string>("FirstName").Equals("Apoorva")).FirstOrDefault();
+            recordData["state"] = "Kerala";
+            Console.WriteLine("***********UpdatedData***************");
+            Console.WriteLine("FirstName:- " + recordData.Field<string>("firstName"));
+            Console.WriteLine("lastName:- " + recordData.Field<string>("lastName"));
+            Console.WriteLine("Address:- " + recordData.Field<string>("address"));
+            Console.WriteLine("City:- " + recordData.Field<string>("city"));
+            Console.WriteLine("State:- " + recordData.Field<string>("state"));
+            Console.WriteLine("zip:- " + Convert.ToInt32(recordData.Field<int>("zip")));
+            Console.WriteLine("phoneNumber:- " + Convert.ToDouble(recordData.Field<Double>("phoneNumber")));
+            Console.WriteLine("eMail:- " + recordData.Field<string>("eMail"));
+            Console.WriteLine("***************");
+
+            return dataTable;
+        }
+
+    }
+}
